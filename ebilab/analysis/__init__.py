758654e0ea9c6361e267e18dd67d20ed846bddba
Ebihara Yusuke <yusuke@ebihara.me>
2024-10-18 16:19:31 +0900

    misc: add warning message to ebilab.analysis / ebilab.analysis2

--- ebilab/analysis/__init__.py
+++ ebilab/analysis/__init__.py
@@ -2,6 +2,9 @@ from ._actions import agg_df_plotter, df_action, df_plotter
 from ._preprocess import original
 from ._process import aggregate, from_df, input, output
 
+import warnings
+warnings.warn("ebilab.analysis package is still in development and may not be stable. You are recommended to lock the version if you are using it in production.", UserWarning)
+
 __all__ = [
     "from_df",
     "input",
