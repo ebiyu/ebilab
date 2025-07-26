8e7f603f96439447c023383d7b32ebea67fe1928
Ebihara Yusuke <yusuke@ebihara.me>
2025-07-26 13:50:14 +0900

    remove!: remove v2 implementation

--- ebilab/analysis2/__init__.py
+++
@@ -1,7 +0,0 @@
-import warnings
-
-warnings.warn(
-    "ebilab.analysis2 package is no longer maintained and will be removed in the future."
-    "You are recommended to lock the version if you are using it.",
-    UserWarning,
-)
