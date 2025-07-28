356a850c8dff085582a6e25ab2a162f95128fd4b
Ebihara Yusuke <yusuke@ebihara.me>
2025-07-28 14:13:40 +0900

    remove!: remove `ebilab.analysis` `ebilab.analysis2` `ebilab.experiment`

--- ebilab/analysis2/tests/test_options.py
+++
@@ -1,9 +0,0 @@
-from ebilab.analysis2.options import BoolField, FloatField, IntField, SelectField, StrField
-
-
-def test_make_instance():
-    FloatField(default=1.0)
-    SelectField(choices=["a", "b"])
-    IntField(default=1)
-    StrField(default="test")
-    BoolField(default=True)
