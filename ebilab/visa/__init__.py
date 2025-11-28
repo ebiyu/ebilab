6cdd9b735ae2cd8f3030fabf70f4d44af27f03ae
Ebihara Yusuke <yusuke@ebihara.me>
2025-11-28 10:14:48 +0900

    feat(device): add 34465A

--- ebilab/visa/__init__.py
+++ ebilab/visa/__init__.py
@@ -5,6 +5,7 @@ Classes to control experimental devices
 from .devices.A707 import A707
 from .devices.E4980 import E4980
 from .devices.K34411A import K34411A
+from .devices.K34465A import K34465A
 from .manager import VisaDevice
 
-__all__ = ["VisaDevice", "K34411A", "A707", "E4980"]
+__all__ = ["VisaDevice", "K34411A", "K34465A", "A707", "E4980"]
---
+++ ebilab/visa/devices/K34465A.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from .K34411A import K34411A
+
+
+class K34465A(K34411A):
+    _idn_pattern = "34465A"
